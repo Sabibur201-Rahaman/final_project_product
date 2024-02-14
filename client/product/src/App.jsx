2b131035c50cbdd8a070f625d90ace2b230b269b
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2024-02-14 17:23:24 +0530

    route is applied

--- client/product/src/App.jsx
+++ client/product/src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-
+import React, { Fragment } from 'react'
+import  { BrowserRouter,Route,Routes } from 'react-router-dom'
+import DashBoardPage from './pages/DashBoardPage';
+import BrandPage from './pages/BrandPage';
 function App() {
   return (
-    <div>
-      <h1>welcome to our productmanagement project</h1>
-    </div>
+    <Fragment>
+<BrowserRouter>
+<Routes>
+<Route exact path='/' element={<DashBoardPage/>}/>
+<Route exact path='/brand' element={<BrandPage/>}/>
+
+
+</Routes>
+</BrowserRouter>
+    </Fragment>
   )
 }
 
--- client/product/src/pages/DashBoardPage.jsx
+++ client/product/src/pages/DashBoardPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function DashBoardPage() {
   return (
     <div>
-      
+      <h1>this is my DashBoard</h1>
     </div>
   )
 }
